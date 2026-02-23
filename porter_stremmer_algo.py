ad1cc4880f2a6e8e5d612f46370f144a4fe4a4eb
Mohitha-Pallapu <mohithapallapu126@gmail.com>
2026-02-23 21:36:29 +0530

    Create porter_stremmer_algo.py

---
+++ porter_stremmer_algo.py
@@ -0,0 +1,11 @@
+#porter stremmer algorithm
+words=['swimming','played','cats','happily']
+def porter(word):
+    suffixes=['ing','ed','s','ly']
+    for suffix in suffixes:
+        if word.endswith(suffix):
+            return word[:-len(suffix)]
+    return word
+stems =[porter(w) for w in words]
+print(stems)
+#output: ['swimm', 'play', 'cat', 'happi']
